1222dd0f8f70b9d3320793d50c6c34c903265894
Rossiel Carranza <rossielcarranza@gmail.com>
2020-05-25 17:40:24 -0300

    Add edit and update
    - Flash.notice
    - Change root to art index
    - New partial

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -13,18 +13,34 @@ class ArticlesController < ApplicationController
      @article = Article.new
  
   end
-  def create
-   
+
+  def create   
     @article = Article.new(article_params)
-
-   
     @article.save
-    redirect_to article_path(@article)
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
+    redirect_to article_path(@article)
   end
+
   def destroy
     @article=Article.find(params[:id])
     @article.destroy
-    redirect_to Article    
+
+    flash.notice = "Article '#{@article.title}' Destroyed!"
+    redirect_to Article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 end
