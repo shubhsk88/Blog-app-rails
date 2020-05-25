c36597bae79577e68820d4316841724165a53ddb
shubhs <shubham88singh@gmail.com>
2020-05-26 01:51:56 +0530

    Adiing the creating and delete functionality

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+include ArticlesHelper
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
@@ -5,5 +7,24 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+     @article = Article.new
+ 
+  end
+  def create
+   
+    @article = Article.new(article_params)
+
+   
+    @article.save
+    redirect_to article_path(@article)
+
+  end
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect_to Article    
   end
 end
